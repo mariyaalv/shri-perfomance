3f39a82263c897dc0e3c24a256b8510aa8ae5af7
mariyaalv <pavlovskayama.maria@yandex.ru>
2025-07-02 23:07:49 +0300

    исправление Footer

--- src/index.jsx
+++ src/index.jsx
@@ -431,31 +431,31 @@ function Main() {
 
 function Footer() {
   return (
-    <footer class="footer">
-      More actions
-      <ul class="footer__list">
-        <li class="footer__item">
-          <a class="footer__link" href="/">
+    <footer className="footer">
+      <ul className="footer__list">
+        <li className="footer__item">
+          <a className="footer__link" href="/">
             Помощь
           </a>
         </li>
-        <li class="footer__item">
-          <a class="footer__link" href="/">
+        <li className="footer__item">
+          <a className="footer__link" href="/">
             Обратная связь
           </a>
         </li>
-        <li class="footer__item">
-          <a class="footer__link" href="/">
+        <li className="footer__item">
+          <a className="footer__link" href="/">
             Разработчикам
           </a>
         </li>
-        <li class="footer__item">
-          <a class="footer__link" href="/">
+        <li className="footer__item">
+          <a className="footer__link" href="/">
             Условия использования
           </a>
         </li>
       </ul>
-      <div class="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
+
+      <div className="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
     </footer>
   );
 }
@@ -468,4 +468,3 @@ root.render(
     <Footer />
   </>
 );
-
