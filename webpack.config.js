05c8e18aeeb02a8d65ba39ed53ad86fea089be05
mariyaalv <pavlovskayama.maria@yandex.ru>
2025-07-02 21:24:23 +0300

    донастройка вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.jsx",
@@ -32,6 +33,18 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/assets",
+          to: "assets",
+        },
+        {
+          from: "src/styles.css",
+          to: "styles.css",
+        }
+      ],
     }),
   ],
   mode: "development",
